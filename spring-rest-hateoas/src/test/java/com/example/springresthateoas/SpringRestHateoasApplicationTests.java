82bb4fa50ca99d8c35363b5f2eccb1b05b5b198b
rkuma318 <rkuma318@LHTU05CG7493V4N.ms.ds.uhc.com>
2020-03-13 21:31:20 +0530

    Initial Commit

---
+++ spring-rest-hateoas/src/test/java/com/example/springresthateoas/SpringRestHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springresthateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestHateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
